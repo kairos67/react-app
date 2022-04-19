8e91d880c72c15c6c72fe2e98424fdb94aeccfe5
Kairos Jeong <kairosys@gmail.com>
2022-04-19 16:52:23 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let posts = { color: "cyan", fontSize: "50px" };
+  function 함수() {
+    return "   개발블로그";
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div style={posts} className={posts}>
+          개발 Blog
+        </div>
+      </div>
+      <img src={logo} />
+      <h4>{posts + 함수()}</h4>
     </div>
   );
 }
