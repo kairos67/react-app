d7555e41a5a6475bc31785186edeed9365b669bb
kairos67 <kairosys@gmail.com>
2022-04-20 18:11:09 +0900

    333

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
 import logo from "./logo.svg";
 import "./App.css";
-
-function App() {
-  let posts = { color: "cyan", fontSize: "50px" };
-  function 함수() {
-    return "   개발블로그";
-  }
+// 컴포넌트
+function Header() {
   return (
-    <div className="App">
-      <div className="black-nav">
-        <div style={posts} className={posts}>
-          개발 Blog
-        </div>
-      </div>
-      <img src={logo} />
-      <h4>{posts + 함수()}</h4>
+    <header>
+      <h1>
+        <a href="/">WEB</a>
+      </h1>
+    </header>
+  );
+}
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href="/read/1">html</a>
+        </li>
+        <li>
+          <a href="/read/2">css</a>
+        </li>
+        <li>
+          <a href="/read/3">js</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+function Article() {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Hello,WEB~~~
+    </article>
+  );
+}
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Header></Header>
+      <Header></Header>
+      <Nav></Nav>
+      <Article>111222222222222222</Article>
     </div>
   );
 }
-
+//https://youtu.be/t9e3hMJ_s-c
 export default App;
