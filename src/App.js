84212c3ccb9f26da46e987c82b516cc11c6e96c9
kairos67 <kairosys@gmail.com>
2022-04-21 19:51:44 +0900

    event commit in Home

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import logo from "./logo.svg";
 import "./App.css";
 // 컴포넌트
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
+
 function Header(props) {
   console.log('props', props, props.title)
   return (
     <header>
       <h1>
-        <a href="/">{props.title}</a>
+        <a href="/" onClick={(event)=>{
+          event.preventDefault();  
+          props.onChangeMode();        
+        }}>{props.title}</a>
       </h1>
     </header>
   );
 }
+
 function Nav(props) {
   const lis = [
   ]
   for (let i = 0; i < props.topics.length; i++) {
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>);
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/' + t.id} onClick={event=>{
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+      }}>{t.title}</a></li>);
   }
   return (
     <nav>
@@ -26,14 +43,7 @@ function Nav(props) {
     </nav>
   );
 }
-function Article(props) {
-  return (
-    <article>
-      <h2>{props.title}</h2>
-      {props.body}
-    </article>
-  );
-}
+
 function App() {
   const topics = [
     { id: 1, title: 'html', body: 'html is...' },
@@ -42,11 +52,16 @@ function App() {
   ]
   return (
     <div>
-      <Header title="WEB"></Header>
-      <Nav topics={topics}></Nav>
+      <Header title="WEB" onChangeMode={()=>{
+        alert('Header');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}></Nav>
       <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
   );
 }
-//https://youtu.be/t9e3hMJ_s-c
+//6.event  https://youtu.be/t9e3hMJ_s-c
+//7.state https://youtu.be/vmunrKR0uOU
 export default App;
