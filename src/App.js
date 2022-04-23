2f7f0fb9a000bca936a75d0d35ae25f13cf68e46
kairos67 <kairosys@gmail.com>
2022-04-23 23:53:19 +0900

    state commit in Home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import {useState} from 'react'
 // 컴포넌트
 function Article(props) {
   return (
@@ -32,7 +33,7 @@ function Nav(props) {
     lis.push(<li key={t.id}>
       <a id={t.id} href={'/read/' + t.id} onClick={event=>{
         event.preventDefault();
-        props.onChangeMode(event.target.id);
+        props.onChangeMode(Number(event.target.id));
       }}>{t.title}</a></li>);
   }
   return (
@@ -45,23 +46,40 @@ function Nav(props) {
 }
 
 function App() {
+  const [mode,setMode] = useState('READ');
+  const [id, setId] = useState(null);
   const topics = [
     { id: 1, title: 'html', body: 'html is...' },
     { id: 2, title: 'css', body: 'css is...' },
     { id: 3, title: 'javascript', body: 'js is...' }
   ]
+  let content = null;
+  if(mode =='WELCOME'){
+    content =  <Article title="Welcome" body="Hello, WEB"></Article>;
+  }else if(mode == 'READ'){
+    let title, body = null;
+    for(let i=0; i<topics.length; i++){
+      if(topics[i].id === id){
+        title=topics[i].title;
+        body=topics[i].body;
+      }
+    }
+    content=<Article title={title} body={body}></Article>
+  }
   return (
     <div>
       <Header title="WEB" onChangeMode={()=>{
-        alert('Header');
+        setMode('WELCOME');
       }}></Header>
-      <Nav topics={topics} onChangeMode={(id)=>{
-        alert(id);
+      <Nav topics={topics} onChangeMode={(_id)=>{
+        setMode('READ');
+        setId(_id);
       }}></Nav>
-      <Article title="Welcome" body="Hello, WEB"></Article>
+      {content}
     </div>
   );
 }
 //6.event  https://youtu.be/t9e3hMJ_s-c
 //7.state https://youtu.be/vmunrKR0uOU
+//8.create https://youtu.be/kctNCMFxciQ
 export default App;
